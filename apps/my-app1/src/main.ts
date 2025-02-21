8f7ac0c7ca92fa6947ea453933906e523839cfef
Marcin <marcin.lazar@pm.me>
2025-02-21 16:15:52 +0100

    fet: emp

--- apps/my-app1/src/main.ts
+++ apps/my-app1/src/main.ts
@@ -6,4 +6,4 @@ bootstrapApplication(AppComponent, appConfig).catch((err) =>
   console.error(err)
 );
 
-console.log('wxawssaxxx2xx2xxxxxxxxxxxxsxxxxx')
+console.log('wxawssaxxx2xx2xxxxxxxxxxxxsxxxxxs')
